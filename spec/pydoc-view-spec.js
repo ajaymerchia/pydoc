8efb66c0044939d7144d42a0f03496fb96666352
Ajay Raj Merchia <amerchia@moveworks.ai>
2020-04-11 14:20:35 -0700

    v1

---
+++ spec/pydoc-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import PydocView from '../lib/pydoc-view';
+
+describe('PydocView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
---
+++ test.py
@@ -0,0 +1,30 @@
+def fn_with_ret() -> int:
+    i = 0
+    p = 2
+    return i + p
+
+def fn_with_params(var1: str, var2: Optional[int]) -> None:
+    var2 *= 2
+    var1 += var1
+    print([var1, var2, var1 + var2])
+
+def fn_with_params_and_ret(var1: str, var2: int) -> list:
+    var2 *= 2
+    var1 += var1
+
+    return [var1, var2, var1 + var2]
+
+def bad_formatted_fn(var2, var3):
+    print('oops')
+    print("I forgot both type specifications")
+    pass
+
+def bad_formatted_fn(var2: str, var3: int):
+    print('oops')
+    print("I forgot type specifications for return type")
+    pass
+
+def bad_formatted_fn(var2: str, var3) -> None:
+    print('oops')
+    print("I forgot type specifications for params")
+    pass
