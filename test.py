c686737f363cda170420c333e2c75f2381f2f2f0
Ajay Raj Merchia <amerchia@moveworks.ai>
2020-04-11 17:05:59 -0700

    Handle all kinds of Variables

--- test.py
+++ test.py
@@ -8,11 +8,19 @@ def fn_with_params(var1: str, var2: Optional[int]) -> None:
     var1 += var1
     print([var1, var2, var1 + var2])
 
+def fn_with_params(var1: str="hello, world", var2: double = 0.567) -> None:
+    var2 *= 2
+    var1 += var1
+    print([var1, var2, var1 + var2])
+
 def fn_with_params_and_ret(var1: str, var2: int) -> list:
     var2 *= 2
     var1 += var1
 
     return [var1, var2, var1 + var2]
+
+def fn_with_params_ridiculous(var1: dict={'hel\"l{o': ('wo\"rl}d', '4', 3)}, var2: double = 0.567) -> double:
+    return var1["he\"ll{o"][2] + var2
 
 def bad_formatted_fn(var2, var3):
     print('oops')
